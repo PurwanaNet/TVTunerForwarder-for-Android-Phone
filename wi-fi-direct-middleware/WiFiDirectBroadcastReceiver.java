cd0ce86645ad55339e63f89266ee5ee5c0139d64
purwana.net <101954284+PurwanaNet@users.noreply.github.com>
2025-03-12 09:22:50 +0700

    Create WiFiDirectBroadcastReceiver.java

---
+++ wi-fi-direct-middleware/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,47 @@
+package com.example.wifidirectapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private MainActivity activity;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Log.d("WiFiDirect", "Wi-Fi Direct is enabled");
+            } else {
+                Log.d("WiFiDirect", "Wi-Fi Direct is disabled");
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            if (manager != null) {
+                manager.requestPeers(channel, new WifiP2pManager.PeerListListener() {
+                    @Override
+                    public void onPeersAvailable(WifiP2pDeviceList peerList) {
+                        for (WifiP2pDevice device : peerList.getDeviceList()) {
+                            Log.d("WiFiDirect", "Found peer: " + device.deviceName);
+                        }
+                    }
+                });
+            }
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            Log.d("WiFiDirect", "Wi-Fi Direct connection state changed");
+        }
+    }
+}
